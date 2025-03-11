917a52902aa1a1047de76c5a94bed53878375616
unknown <raymond.huynh.to@gmail.com>
2025-03-11 11:55:47 -0400

    add working HDMI demo project

---
+++ Nexys-Video-HDMI-2018.2-2/vivado_proj/Nexys-Video-HDMI.sdk/Nexys-Video-HDMI/Debug/src/display_ctrl/display_ctrl.d
@@ -0,0 +1,64 @@
+src/display_ctrl/display_ctrl.o src/display_ctrl/display_ctrl.o: \
+ ../src/display_ctrl/display_ctrl.c ../src/display_ctrl/display_ctrl.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_types.h \
+ ../src/display_ctrl/vga_modes.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xaxivdma.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xaxivdma_hw.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_types.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_io.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_printf.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xparameters.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/bspconfig.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/mb_interface.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_assert.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_exception.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xaxivdma_i.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xdebug.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xstatus.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xvtc.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xvtc_hw.h \
+ ../src/display_ctrl/../dynclk/dynclk.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xdebug.h \
+ ../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_io.h
+
+../src/display_ctrl/display_ctrl.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_types.h:
+
+../src/display_ctrl/vga_modes.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xaxivdma.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xaxivdma_hw.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_types.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_io.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_printf.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xparameters.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/bspconfig.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/mb_interface.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_assert.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_exception.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xaxivdma_i.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xdebug.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xstatus.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xvtc.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xvtc_hw.h:
+
+../src/display_ctrl/../dynclk/dynclk.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xdebug.h:
+
+../../Nexys-Video-HDMI_bsp/microblaze_0/include/xil_io.h:
