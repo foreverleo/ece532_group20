f1c2f0293cdfbcbf226d8998e6ff436bb72f271b
Lizhi Li <98568106+foreverleo@users.noreply.github.com>
2025-03-20 18:42:38 -0400

    Delete Nexys-Video-HDMI-2018.2-2 directory

--- Nexys-Video-HDMI-2018.2-2/vivado_proj/Nexys-Video-HDMI.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_nexys-video-hdmi.elf_on_local.tcl
+++
@@ -1,8 +0,0 @@
-connect -url tcp:127.0.0.1:3121
-configparams mdm-detect-bscan-mask 2
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys Video 210276690173B"} -index 0
-rst -processor
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys Video 210276690173B"} -index 0
-dow C:/Users/Raymond/Downloads/Nexys-Video-HDMI-2018.2-xy/Nexys-Video-HDMI-2018.2-2/vivado_proj/Nexys-Video-HDMI.sdk/Nexys-Video-HDMI/Debug/Nexys-Video-HDMI.elf
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys Video 210276690173B"} -index 0
-con
